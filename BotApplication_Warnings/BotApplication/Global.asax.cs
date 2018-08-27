fbe84f38c57db203befbbfca8b71d4dfaa7536b8
LucioMSP <luciomsp@geeks.ms>
2018-08-27 11:38:23 -0500

    Codigo ejemplo Bot Application Warnings - Solved

---
+++ BotApplication_Warnings/BotApplication/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using System.Web.Http;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+
+namespace BotApplication
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            var store = new InMemoryDataStore();
+
+            Conversation.UpdateContainer(
+                       builder =>
+                       {
+                           builder.Register(c => store)
+                                     .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                                     .AsSelf()
+                                     .SingleInstance();
+
+                           builder.Register(c => new CachingBotDataStore(store,
+                                      CachingBotDataStoreConsistencyPolicy
+                                      .ETagBasedConsistency))
+                                      .As<IBotDataStore<BotData>>()
+                                      .AsSelf()
+                                      .InstancePerLifetimeScope();
+
+
+                       });
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
